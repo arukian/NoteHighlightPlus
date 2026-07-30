40685af69b6fba8177f532d38de40e6537211b6e
arukian <dead_sun@hotmail.es>
2026-07-29 21:27:49 -0600

    created for testing contextual preview

---
+++ NoteHighlightAddin/highlight/Preview/Services/IPreviewSampleCodeService.cs
@@ -0,0 +1,16 @@
+﻿using NoteHighlightAddin.Highlighting.KeywordGroups;
+using System.Runtime.InteropServices;
+
+namespace NoteHighlightAddin.Highlighting.Preview.Services
+{
+    /// <summary>
+    /// Generates contextual source code for the preview.
+    /// </summary>
+    [ComVisible(false)]
+    public interface IPreviewSampleCodeService
+    {
+        string Generate(
+            EditableLanguageConfiguration configuration,
+            KeywordGroupConfiguration selectedGroup);
+    }
+}
